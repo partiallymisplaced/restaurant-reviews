31f893a7ba71a58ea4e3747190522e5473c9a896
Nevena Stoeva <nevena.stoeva@gmail.com>
2018-09-12 16:56:24 -0700

    Adds new styling to content, adds better semantic tags

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ var markers = []
  * Fetch neighborhoods and cuisines as soon as the page is loaded.
  */
 document.addEventListener('DOMContentLoaded', (event) => {
-  initMap(); // added 
+  initMap(); // added
   fetchNeighborhoods();
   fetchCuisines();
 });
@@ -163,20 +163,20 @@ createRestaurantHTML = (restaurant) => {
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
   li.append(name);
+
+  const address = document.createElement('address');
+  address.innerHTML = restaurant.address;
+  li.append(address);
 
   const neighborhood = document.createElement('p');
   neighborhood.innerHTML = restaurant.neighborhood;
   li.append(neighborhood);
 
-  const address = document.createElement('p');
-  address.innerHTML = restaurant.address;
-  li.append(address);
-
   const more = document.createElement('a');
-  more.innerHTML = 'View Details';
+  more.innerHTML = 'See Reviews';
   more.href = DBHelper.urlForRestaurant(restaurant);
   li.append(more)
 
@@ -197,7 +197,7 @@ addMarkersToMap = (restaurants = self.restaurants) => {
     self.markers.push(marker);
   });
 
-} 
+}
 /* addMarkersToMap = (restaurants = self.restaurants) => {
   restaurants.forEach(restaurant => {
     // Add marker to the map
@@ -208,4 +208,3 @@ addMarkersToMap = (restaurants = self.restaurants) => {
     self.markers.push(marker);
   });
 } */
-
