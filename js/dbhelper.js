4676ab252e31d640355c61d78912f773061a9424
Nevena Stoeva <nevena.stoeva@gmail.com>
2018-09-12 19:07:24 -0700

    Adds custom map marker

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -1,6 +1,12 @@
 /**
  * Common database helper functions.
  */
+
+ let customIcon = L.icon({
+   iconUrl: '../img/marker-icon.png',
+   iconSize: [24, 36]
+ });
+
 class DBHelper {
 
   /**
@@ -157,25 +163,14 @@ class DBHelper {
    * Map marker for a restaurant.
    */
    static mapMarkerForRestaurant(restaurant, map) {
-    // https://leafletjs.com/reference-1.3.0.html#marker  
+    // https://leafletjs.com/reference-1.3.0.html#marker
     const marker = new L.marker([restaurant.latlng.lat, restaurant.latlng.lng],
-      {title: restaurant.name,
+    { icon: customIcon,
+      title: restaurant.name,
       alt: restaurant.name,
       url: DBHelper.urlForRestaurant(restaurant)
-      })
+    })
       marker.addTo(newMap);
     return marker;
-  } 
-  /* static mapMarkerForRestaurant(restaurant, map) {
-    const marker = new google.maps.Marker({
-      position: restaurant.latlng,
-      title: restaurant.name,
-      url: DBHelper.urlForRestaurant(restaurant),
-      map: map,
-      animation: google.maps.Animation.DROP}
-    );
-    return marker;
-  } */
-
+  }
 }
-
