37706f0f6f208718a8d9115ba2d803b7357a79e0
Nevena Stoeva <nevena.stoeva@gmail.com>
2018-09-14 15:56:40 -0700

    Adds accessibility improvements

--- js/main.js
+++ js/main.js
@@ -129,7 +129,7 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
-  image.alt = "An image of "+restaurant.name;
+  image.setAttribute('alt','');
   li.append(image);
 
   const name = document.createElement('h2');
@@ -147,6 +147,7 @@ createRestaurantHTML = (restaurant) => {
   const more = document.createElement('a');
   more.innerHTML = 'See Reviews';
   more.href = DBHelper.urlForRestaurant(restaurant);
+  more.setAttribute('role', 'button');
   li.append(more)
 
   return li
